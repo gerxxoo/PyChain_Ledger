f541d480f0e44c4266188323d3fb477dc5c1a989
gerxxoo <gromec00@csu.fullerton.edu>
2024-04-24 22:58:03 -0700

    pushing files

--- pychain.py
+++ pychain.py
@@ -72,7 +72,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
+    record: Record 
 
     creator_id: int
     prev_hash: str = "0"
@@ -170,19 +170,19 @@ pychain = setup()
 
 # @TODO:
 # Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
+# input_data = st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-input_sender = st.text_input('Sender')
+sender = st.text_input('Sender Address')
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-input_receiver = st.text_input('Receiver')
+receiver = st.text_input('Receiver Address')
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-input_amount = st.text_input('Amount')
+amount = st.text_input('Amount')
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
@@ -193,7 +193,8 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
+        # data=input_data,
+        record = Record(sender, receiver, amount),
         creator_id=42,
         prev_hash=prev_block_hash
     )
